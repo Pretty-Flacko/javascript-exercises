cc18b41f63a12ed7b810740ca9f37e5d11929f47
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-10-11 15:50:07 +0400

    Sixth exercise done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output = output.concat(string);
+    }
+    return output;
+    
+}
 
-};
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
