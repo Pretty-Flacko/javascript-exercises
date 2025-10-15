7d96aa9dfaa70f42b7dfd16be30381d90d17d5f7
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-10-16 03:28:12 +0400

    Sixteenth exercise done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        const maxAge = getAge(
+            oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const personAge = getAge(
+            person.yearOfBirth, person.yearOfDeath
+        );
+        if (maxAge < personAge) {
+            return person;
+        } else return oldestPerson;
+    });
 };
 
 // Do not edit below this line
