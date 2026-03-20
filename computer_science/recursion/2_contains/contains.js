42f9f8f22ddfd5be2a45af453dd6c5e3173f74bf
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-03-20 21:11:45 +0400

    Second exercise done

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,16 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  for (const key in obj) {
+    const current = obj[key];
+
+    if (current === value || (Number.isNaN(current) && Number.isNaN(value)))
+      return true;
+
+    if (current && typeof current === "object") {
+      if (contains(current, value)) return true;
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
