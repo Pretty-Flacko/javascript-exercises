54912d17cceaa4b944733a9ab9ee9011b0219663
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-03-27 19:06:53 +0400

    Third exercise done

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,23 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data) {
+  if (data === null || typeof data !== "object") return undefined;
+  let count = 0;
+
+  function helper(value) {
+    if (Number.isInteger(value)) {
+      count++;
+      return;
+    }
+
+    if (value !== null && typeof value === "object") {
+      for (const key in value) {
+        helper(value[key]);
+      }
+    }
+  }
+
+  helper(data);
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
