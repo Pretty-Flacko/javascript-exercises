4c8488ebf63f6cf73715d83bc222cec5d325b573
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-03-27 19:31:38 +0400

    Fifth exercise done

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,19 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) return [1];
+
+  const prev = pascal(n - 1);
+  const row = [];
+
+  row.push(1);
+
+  for (let i = 0; i < prev.length - 1; i++) {
+    row.push(prev[i] + prev[i + 1]);
+  }
+
+  row.push(1);
+
+  return row;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
