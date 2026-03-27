9bc9a8e4e794aae083e7db04e41ea5a3cb20efa7
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-03-27 19:19:32 +0400

    Fourth exercise done

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,25 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length === 0) return [[]];
+
+  const result = [];
+
+  function backtrack(path, remaining) {
+    if (remaining.length === 0) {
+      result.push(path);
+      return;
+    }
+
+    for (let i = 0; i < remaining.length; i++) {
+      backtrack(
+        [...path, remaining[i]],
+        [...remaining.slice(0, i), ...remaining.slice(i + 1)],
+      );
+    }
+  }
+
+  backtrack([], arr);
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
